2c1af25695b590ded9e066bf216310b72ec17c1d
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2022-10-13 23:05:33 -0700

    phase 2

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,23 @@ const removeUser = () => {
     type: REMOVE_USER,
   };
 };
+// TODO why data not data.user?
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
