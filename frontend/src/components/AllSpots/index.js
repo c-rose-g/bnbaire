28da96d264c20378ad0f9106ba26c3073cf2c15e
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2022-10-16 21:41:25 -0700

    Update Splash page

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -6,27 +6,42 @@ import { NavLink } from 'react-router-dom';
 function AllSpots() {
 	//  returning spot image, spot name?, price, rating, city/state(location), dates?
 	const spotsSelector = useSelector((state) => Object.values(state.allSpots));
-  console.log('this is spotsSelector',spotsSelector)
-  // const spotsArray = Object.values(spotsSelector);
-  const dispatch = useDispatch();
-  useEffect(() =>{
-    dispatch(thunkLoadSpots())
-  },[dispatch])
+	console.log('this is spotsSelector', spotsSelector);
+	// const spotsArray = Object.values(spotsSelector);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(thunkLoadSpots());
+	}, [dispatch]);
+
+  const starEmoji = (num) => {
+    let stars= '';
+    for(let i = 0; i < Math.floor(num); i++) {
+      stars += '🌟'
+    }
+    return stars
+  }
 	return (
-    <div className='please work papa'>
-    {spotsSelector.map(spot => {
-      return(
-        <NavLink key={spot.name} to={`/api/spots/${spot.id}`}>
-        <div>
-          <img src={spot.previewImage} alt='spot image'></img>
-        </div>
+		<div className="spots-container">
+			{spotsSelector.map((spot) => {
+				return (
+					<NavLink key={spot.name} to={`/api/spots/${spot.id}`}>
+						<div className='spot-card'>
+            <div className='img-card'>
+							<img src={spot.previewImage} alt="spot image"></img>
+            </div>
+              <div className='spot-text'>
+                <h2>{spot.city},{spot.state}</h2>
+                <h2> {spot.avgRating} {starEmoji(spot.avgRating) }
+                </h2>
+                <h2>{spot.price} </h2>
+              </div>
+						</div>
+					</NavLink>
+				);
+			})}
 
-        </NavLink>
-      )
-         })}
-    </div>
-
-  )
+		</div>
+	);
 }
 
-export default AllSpots
+export default AllSpots;
