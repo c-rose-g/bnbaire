848ba967237af99e37414541923de8e49731bde7
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2022-10-19 16:30:42 -0700

    Add review reducer

--- frontend/src/store/allSpots.js
+++ frontend/src/store/allSpots.js
@@ -167,20 +167,14 @@ export const deleteSpot = (id) => async(dispatch) =>{
 }
 // normalize array
 function normalizeArray(dataArray) {
-	// professional way to write a short circuit:
-	// check the opposite of thing you want, and return early
-	// prevents you from adding your logic into this code block if it's undefined
-	// need error handling in a function to check if data array is undefined,
+
 	if (!(dataArray instanceof Array))
 		throw new Error('Normalize problem: data invalid');
 
 	const obj = {};
-	//if data is an instance of an array then use array methods
 	dataArray.forEach((el) => {
-		// computed property value [el.id] equal to the resource ele itself(el)
 		obj[el.id] = el;
 	});
-	// going to mutate obj, so we're going to return the obj
 	return obj;
 }
 
---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,88 @@
+import { csrfFetch } from "./csrf";
+// TODO define types
+const CREATE_REVIEW_BY_SPOT_ID = 'reviews/actionCreateReviewBySpotId'
+const ADD_REVIEW_IMAGE_TO_NEW_REVIEW = 'reviews/actionAddReviewImageToNewReview'
+const LOAD_REVIEWS_BY_USER = 'reviews/actionLoadReviewsByUser'
+const LOAD_REVIEWS_BY_SPOT_ID = 'reviews/actionLoadReviewsBySpotId'
+const EDIT_REVIEW_BY_REVIEW_ID = 'reviews/actionEditReviewByReviewId'
+const DELETE_REVIEW_BY_SPOT_ID = 'reviews/actionDeleteReviewBySpotId'
+// TODO define action creators
+export const actionCreateReviewBySpotId = (id) =>({
+  type: CREATE_REVIEW_BY_SPOT_ID,
+  id
+})
+export const actionAddReviewImageToNewReview = (review) =>({
+  type: ADD_REVIEW_IMAGE_TO_NEW_REVIEW,
+  review
+})
+
+export const actionLoadReviewsByUser = (reviews)=>({
+  type: LOAD_REVIEWS_BY_USER,
+  reviews
+})
+export const actionLoadReviewsBySpotId = (reviews) =>({
+  type: LOAD_REVIEWS_BY_SPOT_ID,
+  reviews
+})
+export const actionEditReviewByReviewId =(review) =>({
+  type: EDIT_REVIEW_BY_REVIEW_ID,
+  review
+})
+// use id or spot?
+export const actionDeleteReviewBySpotId = (id) =>({
+  type: DELETE_REVIEW_BY_SPOT_ID,
+  id
+})
+function normalizeArray(dataArray) {
+
+	if (!(dataArray instanceof Array))
+		throw new Error('Normalize problem: data invalid');
+
+	const obj = {};
+	dataArray.forEach((el) => {
+		obj[el.id] = el;
+	});
+	return obj;
+}
+// TODO define thunks
+// ****CREATE********************************************************************************************************************
+export const createReview = () => async(dispatch) => {
+
+}
+export const addReviewImage = () => async(dispatch) => {
+
+}
+// ****READ********************************************************************************************************************
+export const loadReviewsByUserThunk = () => async(dispatch) =>{
+  const response = await csrfFetch(`/api/current`)
+  if(response.ok){
+    const data = await response.json()
+    dispatch(actionLoadReviewsByUser(data))
+    return response
+  }
+}
+// ****UPDATE********************************************************************************************************************
+export const editReview = () => async(dispatch) =>{
+
+}
+// ****DELETE********************************************************************************************************************
+export const deleteReview = () => async(dispatch) => {
+
+}
+// TODO define reducer
+const initialState = {spot:{}}
+
+const reviewsReducer = (state = initialState, action) =>{
+  let newState;
+  switch (action.type) {
+    case LOAD_REVIEWS_BY_USER:{
+      newState = {...state}
+
+      // newState.spot = normalizeArray(action.spots)
+    }
+
+
+    default:
+      return state;
+  }
+}
