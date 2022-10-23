f5047d33ad617c23d220bed87fc3ea2c20127960
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2022-10-22 23:59:09 -0700

    add css to create a spot

--- frontend/src/components/CreateSpot/index.js
+++ frontend/src/components/CreateSpot/index.js
@@ -14,13 +14,10 @@ function CreateSpot() {
 	const [name, setName] = useState('');
 	const [description, setDescription] = useState('');
 	const [price, setPrice] = useState('');
-
 	const [url, seturl] = useState('');
 	const [validationErrors, setValidationErrors] = useState([]);
 	const [frontEndErrors, setFrontEndErrors] = useState([]);
-
 	const userSelector = useSelector((state) => state.session.user);
-
 	const updateAddress = (e) => setAddress(e.target.value);
 	const updateName = (e) => setName(e.target.value);
 	const updateCity = (e) => setCity(e.target.value);
@@ -28,9 +25,8 @@ function CreateSpot() {
 	const updateCountry = (e) => setCountry(e.target.value);
 	const updateDescription = (e) => setDescription(e.target.value);
 	const updatePrice = (e) => setPrice(e.target.value);
-
 	const updateUrl = (e) => seturl(e.target.value);
-
+	// const validatePrice = new RegExp('^(\d*([.,](?=\d{3}))?\d+)+((?!\ 2)[.,]\d\d)?$')
 	useEffect(() => {
 		const errors = [];
 		if (address.length < 1) errors.push('please provide an address.');
@@ -43,7 +39,8 @@ function CreateSpot() {
 		if (description.length > 250)
 			errors.push('description must be less than 255 characters.');
 		if (price < 1) errors.push('please provide a price.');
-		if(isNaN(price)) errors.push('price must be a number.')
+		if (isNaN(price)) errors.push('price must be a number.');
+		// if(!validatePrice.test(price)) errors.push('price must be a valid number.')
 		if (!url.endsWith('png') && !url.endsWith('jpg'))
 			errors.push('image needs to end with .jpg or .png.');
 		if (name.length < 1) errors.push('please provide a name for the spot.');
@@ -78,13 +75,13 @@ function CreateSpot() {
 		if (description.length < 1) {
 			errors.push('please provide description');
 		}
-		if (description.length > 250){
-
+		if (description.length > 250) {
 			errors.push('description must be less than 255 characters.');
 		}
 
 		if (price < 1) errors.push('please provide a price.');
-		if(isNaN(price)) errors.push('price must be a number.')
+		if (isNaN(price)) errors.push('price must be a number.');
+		// if(!validatePrice.test(price)) errors.push('price must be a valid number.')
 		if (!url.endsWith('png') && !url.endsWith('jpg')) {
 			errors.push('image needs to end with .jpg or .png.');
 		}
@@ -122,9 +119,94 @@ function CreateSpot() {
 		}
 	};
 	return (
-		<div className="spot-form-container">
-			<h2 className="text">Open your door to hosting</h2>
-			<form onSubmit={handleSubmit}>
+		<div className="newspot-form-container">
+			<div className="newspot-form-card">
+				<div className="newspot-title">Open your door to hosting</div>
+				<form className='newspot-form' onSubmit={handleSubmit}>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="Name"
+							value={name}
+							onChange={updateName}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="Price"
+							value={price}
+							onChange={updatePrice}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="Address"
+							value={address}
+							onChange={updateAddress}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="City"
+							value={city}
+							onChange={updateCity}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="State"
+							value={state}
+							onChange={updateState}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="Country"
+							value={country}
+							onChange={updateCountry}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="Description"
+							value={description}
+							onChange={updateDescription}
+							required
+						/>
+					</label>
+					<label>
+						<input
+							className="newspot-input"
+							type="text"
+							placeholder="Image URL"
+							value={url}
+							onChange={updateUrl}
+							required
+						/>
+					</label>
+					<button className="newspot-input" type="submit">
+						Create a new spot
+					</button>
+				</form>
 				{validationErrors.length > 0 && (
 					<ul className="errors">
 						{validationErrors.map((validate) => (
@@ -132,57 +214,7 @@ function CreateSpot() {
 						))}
 					</ul>
 				)}
-				<input
-					type="text"
-					placeholder="Name"
-					value={name}
-					onChange={updateName}
-				/>
-				<input
-					type="text"
-					placeholder="Price"
-					value={price}
-					onChange={updatePrice}
-				/>
-				<input
-					type="text"
-					placeholder="Address"
-					value={address}
-					onChange={updateAddress}
-				/>
-				<input
-					type="text"
-					placeholder="City"
-					value={city}
-					onChange={updateCity}
-				/>
-				<input
-					type="text"
-					placeholder="State"
-					value={state}
-					onChange={updateState}
-				/>
-				<input
-					type="text"
-					placeholder="Country"
-					value={country}
-					onChange={updateCountry}
-				/>
-				<input
-					type="text"
-					placeholder="Description"
-					value={description}
-					onChange={updateDescription}
-				/>
-
-				<input
-					type="text"
-					placeholder="Image URL"
-					value={url}
-					onChange={updateUrl}
-				/>
-				<button type="submit">Create a new spot</button>
-			</form>
+			</div>
 		</div>
 	);
 }
